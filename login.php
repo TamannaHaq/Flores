c0ce68f3df31de57ad3aeecec7b4ea92fed243fd
tanvirahmedtagim <tanvirahmedtagim@gmail.com>
2022-10-02 12:13:42 +0600

    second-commit

--- login.php
+++ login.php
@@ -51,7 +51,6 @@ if(isset($_POST['submit'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>login</title>
-   
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
